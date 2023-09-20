96f0b7a2025bec34c7d7aaa83fe199fcd8fbf355
Jemma Marie Macapulay <jemma.macapulay@gmail.com>
2023-09-20 16:51:22 -0400

    Testing changes

--- jest/task1.test.js
+++ jest/task1.test.js
@@ -1,6 +1,4 @@
 const dotenv = require("dotenv");
-// const mongoose = require("mongoose");
-// const { leagueValidation } = require('../utils/leaguesModule.js');
 
 dotenv.config();
 const port = process.env.PORT || 8000;
@@ -8,16 +6,11 @@ const url = `http://localhost:${port}`
 
 describe('Test task 1 functions', function () {
 
-    // beforeAll(async () => {
-    //     dotenv.config();
-    //     await mongoose.connect(process.env.DEV_DB);
-    // });
-
     test('Registration - userName is not defined', async () => {
-        //const reqBody = { userName1: "", password: "" };
+        let data = { userName1: "123", password: "123" }
         fetch(`${url}/api/auth/signup`, {
             method: "POST",
-            body: { userName1: "", password: "" },
+            body: JSON.stringify(data),
             headers: { "Content-Type": "Application/JSON" }
         })
         .then(response => {
@@ -26,10 +19,10 @@ describe('Test task 1 functions', function () {
     });
 
     test('Registration - userName is empty', async () => {
-        //const reqBody = { userName: "", password: "" };
+        let data = { userName: "", password: "123" }
         fetch(`${url}/api/auth/signup`, {
             method: "POST",
-            body: { userName: "", password: "" },
+            body: JSON.stringify(data),
             headers: { "Content-Type": "Application/JSON" }
         })
         .then(response => {
@@ -37,7 +30,174 @@ describe('Test task 1 functions', function () {
         })
     });
 
-    // ADD CLOSE CONNECTION
+    test('Registration - password is not defined', async () => {
+        let data = { userName: "user1", password1: "123" }
+        fetch(`${url}/api/auth/signup`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON" }
+        })
+        .then(response => {
+            expect(response.status).toBe(400)
+        })
+    });
+
+    test('Registration - password is empty', async () => {
+        let data = { userName: "user1", password: "" }
+        fetch(`${url}/api/auth/signup`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON" }
+        })
+        .then(response => {
+            expect(response.status).toBe(400)
+        })
+    });
+
+    test('Registration - username is already in used', async () => {
+        let data = { userName: "test1", password: "123aa&GHad" }
+        fetch(`${url}/api/auth/signup`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON" }
+        })
+        .then(response => {
+            expect(response.status).toBe(400)
+        })
+    });
+
+    test('Registration - password does not meet criteria', async () => {
+        let data = { userName: "user1", password: "123aa" }
+        fetch(`${url}/api/auth/signup`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON" }
+        })
+        .then(response => {
+            expect(response.status).toBe(400)
+        })
+    });
+
+    test('Registration - Successful', async () => {
+        let data = { userName: "user1", password: "123aa&GHad" }
+        await fetch(`${url}/api/auth/signup`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON" }
+        })
+        .then(response => {
+            expect(response.status).toBe(201)
+        })
+    });
+
+    test('Login - userName is not defined', async () => {
+        let data = { userName1: "123", password: "123" }
+        fetch(`${url}/api/auth/login`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON" }
+        })
+        .then(response => {
+            expect(response.status).toBe(400)
+        })
+    });
+
+    test('Login - userName is empty', async () => {
+        let data = { userName: "", password: "123" }
+        fetch(`${url}/api/auth/login`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON" }
+        })
+        .then(response => {
+            expect(response.status).toBe(400)
+        })
+    });
+
+    test('Login - password is not defined', async () => {
+        let data = { userName: "user1", password1: "123" }
+        fetch(`${url}/api/auth/login`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON" }
+        })
+        .then(response => {
+            expect(response.status).toBe(400)
+        })
+    });
+
+    test('Login - password is empty', async () => {
+        let data = { userName: "user1", password: "" }
+        fetch(`${url}/api/auth/login`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON" }
+        })
+        .then(response => {
+            expect(response.status).toBe(400)
+        })
+    });
+
+    test('Login - Username is not found', async () => {
+        let data = { userName: "userx", password: "123aa&GHad" }
+        fetch(`${url}/api/auth/login`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON" }
+        })
+        .then(response => {
+            expect(response.status).toBe(400)
+        })
+    });
+
+    test('Login - Password does not match with db', async () => {
+        let data = { userName: "user1", password: "123aa&GHadx" }
+        fetch(`${url}/api/auth/login`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON" }
+        })
+        .then(response => {
+            expect(response.status).toBe(400)
+        })
+    });
+
+    test('Login - Successful', async () => {
+        let data = { userName: "user1", password: "123aa&GHad" }
+        await fetch(`${url}/api/auth/login`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON" }
+        })
+        .then(response => {
+            expect(response.status).toBe(202)
+        })
+    });
+
+    let user1Token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTBiNGVlZmIzYTQ2MGMyODMzZGYwZjUiLCJpYXQiOjE2OTUyNDE4MTksImV4cCI6MTY5NTI1MjYxOX0.RSia0LRaQlB-VXvnRxRIuv9w3m014-1DDpVhbEHp1Fw"
+    test('Create Note - Empty detail', async () => {
+        let data = { contents: " " }
+        fetch(`${url}/api/notes`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON", "Authorization": user1Token }
+        })
+        .then(response => {
+            expect(response.status).toBe(400)
+        })
+    });
+
+    test('Create Note - Successful', async () => {
+        let data = { contents: "User1 note 2" }
+        await fetch(`${url}/api/notes`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "Application/JSON", "Authorization": user1Token }
+        })
+        .then(response => {
+            expect(response.status).toBe(200)
+        })
+    });
 
 });
 
